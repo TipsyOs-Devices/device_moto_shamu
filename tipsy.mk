2d010838e636e6fbee0429ca61a446ad040ed2f5
Alx31 <alex31.couderc@gmail.com>
2016-12-04 12:54:48 +0100

    tipsy
    Change-Id: Ic058f9bcb57d8531635fa12872c1f18b3750f7a1

--- tipsy.mk
+++ tipsy.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_shamu
+PRODUCT_NAME := tipsy_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
