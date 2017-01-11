4dc73d2bc7e7f4613358e8972d5f151f74e51a6f
ezio84 <brabus84@gmail.com>
2017-01-12 00:01:28 +0100

    Lockscreen charging info: real time values [3/4]
    Change-Id: I0c88bf9210018b5cbf39deddd0fc1214e5c778d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,4 +142,7 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw   \
     device/moto/shamu/cmhw
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 -include vendor/motorola/shamu/BoardConfigVendor.mk
